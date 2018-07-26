f624727ceb7996741cc6c7fdea9633e764094a58
kimjunhee <wnsgml972@gmail.com>
2018-07-26 17:14:33 +0900

    Update SinglePath

--- WebContent/build/js/custom.jun.js
+++ WebContent/build/js/custom.jun.js
@@ -2,8 +2,23 @@ function play(){
 	alert("1");
 }
 function pause(){
-	alert("2");
+    alert('2');
 }
 function stop(){
 	alert("3");
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+        	 callback(xmlHttp.responseText);
+        }           
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+function myCallBack(response){
+	alert(response);
 }

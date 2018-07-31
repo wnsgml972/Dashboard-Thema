f8b18cb8f4e6124ff18956189270abcd4bf0dcc9
kimjunhee <wnsgml972@gmail.com>
2018-07-31 11:26:55 +0900

    Update BW Check

--- WebContent/build/js/custom.jun.js
+++ WebContent/build/js/custom.jun.js
@@ -8,17 +8,3 @@ function stop(){
 	alert("3");
 }
 
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-        	 callback(xmlHttp.responseText);
-        }           
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
-function myCallBack(response){
-	alert(response);
-}
